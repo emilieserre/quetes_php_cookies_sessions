b3604b0982a6df3526a6bb40709061dea83fa38f
Emilie SERRE <emilieserre@hotmail.com>
2019-05-06 17:39:16 +0200

    ajout du bouton déconnexion

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ if (!empty($_GET['add_to_cart'])){
         case "46":
             if (empty($_COOKIE['panier']['pecanNuts'])){
                 setCookie("panier[pecanNuts]", 1);
+                header ("location: index.php");
                 break;
             }
             $_COOKIE['panier']["pecanNuts"]++;
@@ -42,56 +43,76 @@ if (!empty($_GET['add_to_cart'])){
     }
 }
 ?>
-<section class="cookies container-fluid">
-  <div class="row">
-    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-      <figure class="thumbnail text-center">
-        <img src="assets/img/product-46.jpg" alt="cookies choclate chips" class="img-responsive">
-        <figcaption class="caption">
-          <h3>Pecan nuts</h3>
-          <p>Cooked by Penny !</p>
-          <a  href="?add_to_cart=46" class="btn btn-primary">
-            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
-          </a>
-        </figcaption>
-      </figure>
-    </div>
-    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-      <figure class="thumbnail text-center">
-        <img src="assets/img/product-36.jpg" alt="cookies chocolate chips" class="img-responsive">
-        <figcaption class="caption">
-          <h3>Chocolate chips</h3>
-          <p>Cooked by Bernadette !</p>
-          <a  href="?add_to_cart=36" class="btn btn-primary">
-            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
-          </a>
-        </figcaption>
-      </figure>
-    </div>
-    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-      <figure class="thumbnail text-center">
-        <img src="assets/img/product-58.jpg" alt="cookies chocolate chips" class="img-responsive">
-        <figcaption class="caption">
-          <h3>Chocolate cookie</h3>
-          <p>Cooked by Bernadette !</p>
-          <a  href="?add_to_cart=58" class="btn btn-primary">
-            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
-          </a>
-        </figcaption>
-      </figure>
-    </div>
-    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-      <figure class="thumbnail text-center">
-        <img src="assets/img/product-32.jpg" alt="cookies chocolate chips" class="img-responsive">
-        <figcaption class="caption">
-          <h3>M&M's&copy; cookies</h3>
-          <p>Cooked by Penny !</p>
-          <a  href="?add_to_cart=32" class="btn btn-primary">
-            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
-          </a>
-        </figcaption>
-      </figure>
-    </div>
-  </div>
-</section>
+    <section class="cookies container-fluid">
+        <div class="row">
+
+
+
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <figure class="thumbnail text-center">
+                    <img src="assets/img/product-46.jpg" alt="cookies choclate chips" class="img-responsive">
+                    <figcaption class="caption">
+                        <h3>Pecan nuts</h3>
+                        <h4><?= $_COOKIE['panier']['pecanNuts'] ?? '' ?></h4>
+                        <p>Cooked by Penny !</p>
+                        <form role="form" action="" method="GET">
+                            <input type="hidden" name="add_to_cart" value="46" />
+                            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Ajouter au Panier</button>
+                        </form>
+                    </figcaption>
+                </figure>
+            </div>
+
+
+
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <figure class="thumbnail text-center">
+                    <img src="assets/img/product-36.jpg" alt="cookies choclate chips" class="img-responsive">
+                    <figcaption class="caption">
+                        <h3>Chocolate chips</h3>
+                        <h4><?= $_COOKIE['panier']['chocolateChips'] ?? '' ?></h4>
+                        <p>Cooked by Bernadette !</p>
+                        <form role="form" action="" method="GET">
+                            <input type="hidden" name="add_to_cart" value="36" />
+                            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Ajouter au Panier</button>
+                        </form>
+                    </figcaption>
+                </figure>
+            </div>
+
+
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <figure class="thumbnail text-center">
+                    <img src="assets/img/product-58.jpg" alt="cookies choclate chips" class="img-responsive">
+                    <figcaption class="caption">
+                        <h3>Chocolate cookie</h3>
+                        <h4><?= $_COOKIE['panier']['chocolateCookie'] ?? '' ?></h4>
+                        <p>Cooked by Bernadette !</p>
+                        <form role="form" action="" method="GET">
+                            <input type="hidden" name="add_to_cart" value="58" />
+                            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Ajouter au Panier</button>
+                        </form>
+                    </figcaption>
+                </figure>
+            </div>
+
+
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <figure class="thumbnail text-center">
+                    <img src="assets/img/product-32.jpg" alt="cookies choclate chips" class="img-responsive">
+                    <figcaption class="caption">
+                        <h3>M&M's&copy; cookies</h3>
+                        <h4><?= $_COOKIE['panier']['mmsCookies'] ?? '' ?></h4>
+                        <p>Cooked by Penny !</p>
+                        <form role="form" action="" method="GET">
+                            <input type="hidden" name="add_to_cart" value="32" />
+                            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Ajouter au Panier</button>
+                        </form>
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+    </section>
+
+
 <?php require 'inc/foot.php'; ?>
